f18b92488f495e0be514a36e25f358265a651a48
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2024-05-27 12:27:24 +0545

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: '.next',  // Use default directory to avoid issues
+  // distDir: '.next',  // Use default directory to avoid issues
 }
 
 module.exports = nextConfig
